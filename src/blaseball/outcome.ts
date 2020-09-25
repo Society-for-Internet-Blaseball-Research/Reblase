a5032b4bf432040179a2d603e90cb7f78f77de6d
Astrid <me@astr.cc>
2020-09-25 17:12:01 +0200

    Move Peanut outcome last so Peanut names don't mix things up

--- src/blaseball/outcome.ts
+++ src/blaseball/outcome.ts
@@ -20,11 +20,11 @@ export const outcomeTypes: OutcomeType[] = [
     { name: "Reverb", emoji: "\u{1F30A}", search: [/reverb/i], color: "blue" },
     { name: "Feedback", emoji: "\u{1F3A4}", search: [/feedback/i], color: "pink" },
     { name: "Incineration", emoji: "\u{1F525}", search: [/rogue umpire/i], color: "orange" },
-    { name: "Peanut", emoji: "\u{1F95C}", search: [/peanut/i], color: "orange" },
     { name: "Blooddrain", emoji: "\u{1FA78}", search: [/blooddrain/i], color: "purple" },
     { name: "Unstable", emoji: "\u{1F974}", search: [/Unstable/i], color: "blue" },
     { name: "Flickering", emoji: "\u{26A1}", search: [/Flickering/i], color: "blue" },
     { name: "Birds", emoji: "\u{1F426}", search: [/The Birds pecked/i], color: "purple" },
+    { name: "Peanut", emoji: "\u{1F95C}", search: [/peanut/i], color: "orange" },
 ];
 
 export function getOutcomes(evt: GamePayload): Outcome[] {
