91a84f3862916114158599ecb0f893caa29fa593
Astrid <me@astr.cc>
2020-10-05 19:25:40 +0200

    Properly handle circlification overload

--- src/components/Circles.tsx
+++ src/components/Circles.tsx
@@ -8,7 +8,7 @@ interface CirclesProps {
 
 export function Circles({ amount, total, label }: CirclesProps) {
     let circlesStr = "";
-    for (let i = 0; i < total; i++) {
+    for (let i = 0; i < Math.max(amount, total); i++) {
         circlesStr += i < amount ? "\u25c9" : "\u25cb";
     }
     return <span className="text-xl leading-6 ml-2">{circlesStr}</span>;
