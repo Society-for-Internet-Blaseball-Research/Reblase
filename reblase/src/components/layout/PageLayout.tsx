fd2c754c9415e7fd6955cbe44adef2a2f4a3c889
Astrid <me@astr.cc>
2021-03-08 14:26:39 +0100

    Update credit

--- reblase/src/components/layout/PageLayout.tsx
+++ reblase/src/components/layout/PageLayout.tsx
@@ -16,7 +16,7 @@ export function PageLayout(props: { children: React.ReactNode }) {
                 </a>
                 .
                 <br />
-                <a href="https://twitter.com/floofstrid">Author</a> (@Ske#6201) |{" "}
+                <a href="https://twitter.com/AReblase">Author</a> (@AReblase) |{" "}
                 <a href="https://github.com/Society-for-Internet-Blaseball-Research/Reblase">GitHub</a> |{" "}
                 <a href="https://discord.gg/XKppCuj">SIBR Discord</a>
             </div>
