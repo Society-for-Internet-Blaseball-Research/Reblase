83d8dbf46238ec393de637a7a6a9422e1c2d7c94
Astrid <me@astr.cc>
2021-04-13 04:28:23 +0200

    okay fine you can have little a early FK unlock, as a treat

--- blaseball-lib/weather.ts
+++ blaseball-lib/weather.ts
@@ -85,7 +85,7 @@ export const allWeatherTypes = [
         // icon: WiStars,
         background: "#ff94ff",
         color: "#fff98a",
-        forbidden: true,
+        forbidden: false,
     },
     {
         id: 9,
