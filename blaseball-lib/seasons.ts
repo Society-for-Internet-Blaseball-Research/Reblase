34f63b11f359b134bb371818ddf43d5b94a52ef1
Lofty Codd <Alastair.Codd@abdynamics.com>
2023-01-23 18:44:20 +0000

    Stop always pointing at season one for experimental games

--- blaseball-lib/seasons.ts
+++ blaseball-lib/seasons.ts
@@ -4,3 +4,5 @@ export const returnedSeasons: Map<number, SeasonID> = new Map([
     [0, "cd1b6714-f4de-4dfc-a030-851b3459d8d1"],
     [1, "7af53acf-1fb9-40e8-96c7-ab8308a353f9"],
 ]);
+
+export const returnedSeasonsById: Map<SeasonID, number> = new Map(Array.from(returnedSeasons, entry => [entry[1], entry[0]]));
