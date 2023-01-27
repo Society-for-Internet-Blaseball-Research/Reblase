6ff12383ddfbc776b5e748be2d2d437b10829386
Lofty Codd <Alastair.Codd@abdynamics.com>
2023-01-27 16:13:23 +0000

    Update weather picker

--- reblase/src/components/elements/WeatherPicker.tsx
+++ reblase/src/components/elements/WeatherPicker.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { selectTheme } from "../../blaseball/select";
 import Select from "react-select";
-import { allWeatherTypes, WeatherType } from "blaseball-lib/weather";
+import { allWeatherTypes, WeatherType, allExperimentalWeatherTypes, ExperimentalWeatherType } from "blaseball-lib/weather";
 import Twemoji from "./Twemoji";
+import { WeatherID } from "blaseball-lib/common";
 
 export interface WeatherPickerProps {
     placeholder?: string;
@@ -31,6 +32,39 @@ export default function WeatherPicker(props: WeatherPickerProps) {
             }}
             onChange={(newItems, _) => {
                 const ids = ((newItems ?? []) as WeatherType[]).map((item) => item.id);
+                if (props.setSelectedWeather) props.setSelectedWeather(ids);
+            }}
+        />
+    );
+}
+
+export interface WeatherPickerExperimentalProps {
+    placeholder?: string;
+    selectedWeather?: WeatherID[];
+    setSelectedWeather?: (weather: WeatherID[]) => void;
+}
+
+export function WeatherPickerExperimental(props: WeatherPickerExperimentalProps) {
+    const items = Array(...allExperimentalWeatherTypes.values()).filter((weather) => !weather.forbidden);
+
+    return (
+        <Select
+            options={items}
+            theme={selectTheme}
+            isMulti={true}
+            placeholder={props.placeholder}
+            value={items.filter((item) => (props.selectedWeather ?? []).indexOf(item.id) !== -1)}
+            getOptionValue={(weather) => weather.name}
+            formatOptionLabel={(weather) => {
+                return (
+                    <>
+                        <Twemoji className="mr-1" emoji={weather.emoji ?? "?"} />
+                        {weather.name}
+                    </>
+                );
+            }}
+            onChange={(newItems, _) => {
+                const ids = ((newItems ?? []) as ExperimentalWeatherType[]).map((item) => item.id);
                 if (props.setSelectedWeather) props.setSelectedWeather(ids);
             }}
         />
